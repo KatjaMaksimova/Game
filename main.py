86a2eb31a66db503e1be26b447566b6878eb96b2
KatjaMaksimova <maksimova.k21@gmail.com>
2023-12-21 10:33:00 +0300

    Дино дышит! Реализован класс NPC.

--- main.py
+++ main.py
@@ -1,27 +1,18 @@
 from const import *
+from data.sprite.NPC import NPC
 import pygame
-import time
 
 pygame.init()
 screen = pygame.display.set_mode((WIDCH, HENGT))
 
-mouse = pygame.image.load(NPC_FOTO["Dino"]["foto"])
-mouse = pygame.transform.scale(mouse, (120, 120))
-mouseX = 0
-mouseY = 200
-idx_foto = 0
-list_foto = NPC_FOTO["Dino"]["fotoBreath_list"]
+nps = NPC(screen=screen)
+
 while True:
     screen.fill((0, 0, 0))
-    pygame.time.delay(30)
-    time.sleep(0.1)
-    idx_foto += 1
+    pygame.time.delay(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    idx_foto %= 6
-    mouse = pygame.image.load(list_foto[idx_foto])
-    mouse = pygame.transform.scale(mouse, (120, 120))
-    screen.blit(mouse, (mouseX, mouseY))
+    nps.breath()
     pygame.display.update()
